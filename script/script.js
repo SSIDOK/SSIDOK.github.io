591f78f7cdef03d59bdfd43bc945f15f0ad6a693
Pavlo <92532233+SSIDOK@users.noreply.github.com>
2023-10-21 19:53:21 +0300

    Update script.js

--- script/script.js
+++ script/script.js
@@ -3,10 +3,22 @@ const iconOpen = document.getElementById('iconOpen')
 const iconClose = document.getElementById('iconClose')
 const mainMenu = document.getElementById('mainMenu')
 
-
-
 iconWrap.addEventListener('click',() => {
 	mainMenu.classList.toggle('hide-menu')
 	iconOpen.classList.toggle('hide')
 	iconClose.classList.toggle('hide')
 })
+
+$(document).ready(function() {
+  $('#mainMenu a, .latest-work a, .up-button a').on('click', function(event) {
+    event.preventDefault();
+
+    var targetSection = $($(this).attr('href'));
+
+    if (targetSection.length) {
+      $('html, body').animate({
+        scrollTop: targetSection.offset().top
+      }, 1000);
+    }
+  });
+});
